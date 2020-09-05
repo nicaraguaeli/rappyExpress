182137ea570220932522cdfd400f67692cb7d3f5
nicaraguaeli <moncadaeli567@gmail.com>
2020-09-05 08:38:34 -0600

    test

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('/register','Auth\RegisterController@register');
 
-Route::group(['middleware' => ['api']], function () {
+Route::group(['middleware' => ['web']], function () {
     // your routes here
     Route::post('/login','Auth\LoginController@login');
 });
