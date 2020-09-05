3f3e20a9d7e5d0a9de89824b9016a8ddbe8a1c53
nicaraguaeli <moncadaeli567@gmail.com>
2020-09-04 20:46:30 -0600

    route

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/register','Auth\RegisterController@register');
 Route::post('/login','Auth\LoginController@login');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
